107eb8fef3da7bfb6e5886ba7072a0c83d00e7e7
Robert Fahl <robfahl1@gmail.com>
2023-04-27 01:07:34 -0400

    Add MinimumDistributionCalculator

---
+++ MobileTools/ViewModels/MinimumDistributionCalculatorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MobileTools.ViewModels;
+
+public partial class MinimumDistributionCalculatorViewModel : ObservableObject
+{
+	[ObservableProperty]
+	int age;
+
+    [ObservableProperty]
+    double minimumDistribution;
+
+    [ObservableProperty]
+    double accountBalance;
+
+    public MinimumDistributionCalculatorViewModel()
+	{}
+
+    [RelayCommand]
+    public void CalculateMinimumDistribution()
+	{
+        // TODO - IRS Guidelines https://www.irs.gov/publications/p590b
+        MinimumDistribution = 999;
+    }
+}
+
---
+++ MobileTools/Views/MinimumDistributionCalculator.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MobileTools.ViewModels;
+
+namespace MobileTools.Views;
+
+public partial class MinimumDistributionCalculator : ContentPage
+{
+	public MinimumDistributionCalculator(MinimumDistributionCalculatorViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+}
