082b5b3ab856d52ecf4ea029821d8ee4560c9134
Robert Fahl <robfahl1@gmail.com>
2023-04-27 01:09:36 -0400

    Use view model for BusinessInvestmentCalculator

---
+++ MobileTools/ViewModels/BusinessInvestmentCalculatorViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MobileTools.ViewModels;
+
+public partial class BusinessInvestmentCalculatorViewModel : ObservableObject
+{
+    [ObservableProperty]
+    int initialInvestment;
+
+    [ObservableProperty]
+    double rateOfReturn;
+
+    [ObservableProperty]
+    double rateOfInflation;
+
+    [ObservableProperty]
+    int investmentPeriodInYears;
+
+    [ObservableProperty]
+    int expectedAnnualCompanyEarningsAtEndOfInvestmentPeriod;
+
+    [ObservableProperty]
+    bool includeRiskFactor;
+
+    [ObservableProperty]
+    double returnOnInvestment;
+
+    [ObservableProperty]
+    double requiredOwnership;
+
+    const double RISK_FACTOR = 0.5;
+
+    public BusinessInvestmentCalculatorViewModel()
+	{}
+
+    [RelayCommand]
+    public void CalculateReturnsAndOwnership()
+    {
+        var returnRate = RateOfReturn * .01 + 1;
+        var inflationRate = RateOfInflation * .01 + 1;
+
+        var returns = InitialInvestment * 1.0;
+        for (var year = 0; year < InvestmentPeriodInYears; year++)
+        {
+            returns = returns * returnRate * inflationRate;
+        }
+
+        var companyAcquisitionPrice = ((ExpectedAnnualCompanyEarningsAtEndOfInvestmentPeriod) * 10);
+        var riskAdjustedAcquisitionPrice = companyAcquisitionPrice * RISK_FACTOR;
+
+        var ownership = (returns / (IncludeRiskFactor ? riskAdjustedAcquisitionPrice : companyAcquisitionPrice)) * 100;
+
+        ReturnOnInvestment = Math.Round(returns);
+        RequiredOwnership = Math.Round(ownership);
+    }
+}
+
--- MobileTools/Views/BusinessInvestmentCalculator.xaml.cs
+++ MobileTools/Views/BusinessInvestmentCalculator.xaml.cs
@@ -1,61 +1,12 @@
-﻿namespace MobileTools.Views;
+﻿using MobileTools.ViewModels;
+
+namespace MobileTools.Views;
 
 public partial class BusinessInvestmentCalculator : ContentPage
 {
-    int initialInvestment = 0;
-    double rateOfReturn = 0;
-    double rateOfInflation = 0;
-    int investmentPeriodInYears = 0;
-    int expectedAnnualCompanyEarningsAtEndOfInvestmentPeriod = 0;
-    const double RISK_FACTOR = 0;
-
-    public BusinessInvestmentCalculator()
+    public BusinessInvestmentCalculator(BusinessInvestmentCalculatorViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
-
-    void OnInitialInvestmentTextChanged(object sender, TextChangedEventArgs e)
-    {
-        initialInvestment = string.IsNullOrWhiteSpace(e.NewTextValue) ? 0 : Int32.Parse(e.NewTextValue);
-    }
-
-    void OnRateOfReturnTextChanged(object sender, TextChangedEventArgs e)
-    {
-        rateOfReturn = string.IsNullOrWhiteSpace(e.NewTextValue) ? 0 : double.Parse(e.NewTextValue);
-    }
-
-    void OnRateOfInflationTextChanged(object sender, TextChangedEventArgs e)
-    {
-        rateOfInflation = string.IsNullOrWhiteSpace(e.NewTextValue) ? 0 : double.Parse(e.NewTextValue);
-    }
-
-    void OnInvestmentPeriodTextChanged(object sender, TextChangedEventArgs e)
-    {
-        investmentPeriodInYears = string.IsNullOrWhiteSpace(e.NewTextValue) ? 0 : Int32.Parse(e.NewTextValue);
-    }
-
-    void OnExpectedCompanyEarningsTextChanged(object sender, TextChangedEventArgs e)
-    {
-        expectedAnnualCompanyEarningsAtEndOfInvestmentPeriod = string.IsNullOrWhiteSpace(e.NewTextValue) ? 0 : Int32.Parse(e.NewTextValue);
-    }
-
-    private void CalculateReturnsAndOwnership(object sender, EventArgs e)
-    {
-        var returnRate = rateOfReturn * .01 + 1;
-        var inflationRate = rateOfInflation * .01 + 1;
-
-        var returns = initialInvestment * 1.0;
-        for (var year = 0; year < investmentPeriodInYears; year++)
-        {
-            returns = returns * returnRate * inflationRate;
-        }
-
-        var companyAcquisitionPrice = ((expectedAnnualCompanyEarningsAtEndOfInvestmentPeriod) * 10);
-        var riskAdjustedAcquisitionPrice = companyAcquisitionPrice * RISK_FACTOR;
-
-        var ownership = (returns / (IncludeRiskFactor.IsChecked ? riskAdjustedAcquisitionPrice : companyAcquisitionPrice)) * 100;
-
-        ReturnOnInvestment.Text = Math.Round(returns).ToString();
-        RequiredOwnership.Text = Math.Round(ownership).ToString();
-    }
 }
