7bcf2c2257829f656359e0ef88c636ec2abbd608
Robert Fahl <robfahl1@gmail.com>
2023-04-27 01:06:54 -0400

    Move MainPage out of Views folder

---
+++ MobileTools/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MobileTools.Views;
+
+namespace MobileTools;
+
+public partial class MainPage : ContentPage
+{
+    CompoundInterestCalculator _compoundInterestCalculator;
+    BusinessInvestmentCalculator _businessInvestmentCalculator;
+    MinimumDistributionCalculator _minimumDistributionCalculator;
+    MortgageCalculator _mortgageCalculator;
+
+    public MainPage(CompoundInterestCalculator cic, BusinessInvestmentCalculator bic, MinimumDistributionCalculator mdc, MortgageCalculator mc)
+	{
+		InitializeComponent();
+        _compoundInterestCalculator = cic;
+        _businessInvestmentCalculator = bic;
+        _minimumDistributionCalculator = mdc;
+        _mortgageCalculator = mc;
+    }
+
+    public async void NavigateToCompoundInterestCalculator(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(_compoundInterestCalculator);
+    }
+
+    public async void NavigateToBusinessInvestmentCalculator(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(_businessInvestmentCalculator);
+	}
+
+    public async void NavigateToMinimumDistributionCalculator(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(_minimumDistributionCalculator);
+    }
+
+    public async void NavigateToMortgageCalculator(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(_mortgageCalculator);
+    }
+}
