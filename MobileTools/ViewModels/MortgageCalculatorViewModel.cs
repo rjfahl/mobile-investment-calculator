86b3d6e828431c3e301e5f7c561ed3b7c5a542bd
Robert Fahl <robfahl1@gmail.com>
2023-04-27 01:07:55 -0400

    Add MortgageCalculator

---
+++ MobileTools/ViewModels/MortgageCalculatorViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MobileTools.ViewModels;
+
+public partial class MortgageCalculatorViewModel : ObservableObject
+{
+	[ObservableProperty]
+	int homeValue;
+
+    [ObservableProperty]
+    int downPayment;
+
+    [ObservableProperty]
+    double interestRate;
+
+    [ObservableProperty]
+    int loanTerm;
+
+    [ObservableProperty]
+    double totalMonthlyPayment;
+
+    public MortgageCalculatorViewModel()
+	{}
+
+    [RelayCommand]
+    public void CalculateMortgage()
+	{
+        // TODO - Zillow Calculator https://www.zillow.com/mortgage-calculator/
+        TotalMonthlyPayment = 999;
+    }
+}
+
---
+++ MobileTools/Views/MortgageCalculator.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MobileTools.ViewModels;
+
+namespace MobileTools.Views;
+
+public partial class MortgageCalculator : ContentPage
+{
+	public MortgageCalculator(MortgageCalculatorViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+}
